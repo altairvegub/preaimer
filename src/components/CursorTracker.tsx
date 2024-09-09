8bd388c6fd7fc02b4201ed8317e7f03ad3f6153a
gub <gub@live.ca>
2024-09-08 23:02:23 -0400

    added new image assets and .editorconfig

--- src/components/CursorTracker.tsx
+++ src/components/CursorTracker.tsx
@@ -2,38 +2,39 @@
 import { useState, useEffect } from 'react';
 
 export default function CursorTracker() {
-	const [position, setPosition] = useState({ x: 0, y: 0 });
-	const [clickPos, setClickPos] = useState({ x: 0, y: 0 });
-	const targetPos = { x: 1284, y: 641 };
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [clickPos, setClickPos] = useState({ x: 0, y: 0 });
+    const targetPos = { x: 1284, y: 641 };
 
-	const handleMouseMove = (event: any) => {
-		setPosition({
-			x: event.clientX,
-			y: event.clientY,
-		});
-	};
+    const handleMouseMove = (event: any) => {
+        setPosition({
+            x: event.clientX,
+            y: event.clientY,
+        });
+    };
 
-	const handleMouseUp = (event: any) => {
-		setClickPos({
-			x: event.clientX,
-			y: event.clientY,
-		})
-	}
+    const handleMouseUp = (event: any) => {
+        setClickPos({
+            x: event.clientX,
+            y: event.clientY,
+        })
+    }
 
-	useEffect(() => {
-		document.addEventListener('mousemove', handleMouseMove);
-		document.addEventListener('mouseup', handleMouseUp);
+    useEffect(() => {
+        document.addEventListener('mousemove', handleMouseMove);
+        //	document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
 
-		return () => {
-			document.removeEventListener('mousemove', handleMouseMove);
-			document.removeEventListener('mouseup', handleMouseUp);
-		};
-	}, []);
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+        };
+    }, []);
 
-	return (
-		<div id='tracker'>
-			<p>Cursor position: {`X: ${position.x}, Y: ${position.y}`}<br />
-				Clicked position: {`X: ${clickPos.x}, Y: ${clickPos.y}`}</p>
-		</div>
-	);
+    return (
+        <div id='tracker'>
+            <p>Cursor position: {`X: ${position.x}, Y: ${position.y}`}<br />
+                Clicked position: {`X: ${clickPos.x}, Y: ${clickPos.y}`}</p>
+        </div>
+    );
 }
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,13 +1,11 @@
 import Image from 'next/image'
 
 export default function Game() {
-
-
-	return (
-		<>
-			<div className="game">
-				<Image src={`/screenshots/bind_1_a.png`} alt="angle" width="1600" height="900" fill={false} />
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className="game">
+                <Image src={`/screenshots/ascent_1.png`} alt="angle" width="1920" height="1080" fill={false} />
+            </div>
+        </>
+    );
 }
