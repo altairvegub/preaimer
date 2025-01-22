7d2d9cbe3134876be610b86360296a4fcc1e8ed5
gub <gub@live.ca>
2025-01-22 00:02:32 -0500

    removed horizontal scrolling and moved button

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -22,8 +22,8 @@ function Game({ x, y }: GameProps) {
 
     return (
         <>
-            <div ref={gameImgRef}>
-                <Image id="myImgId" className="select-none" src={`/screenshots/4k_range.jpg`} alt="angle" width='2560' height='1440' fill={false} style={{ objectFit: "cover" }} />
+            <div ref={gameImgRef} style={{ width: 2560, overflow: 'hidden' }}>
+                <Image id="myImgId" className="select-none" src={`/screenshots/4k_range.jpg`} alt="angle" width='2560' height='1440' fill={false} style={{ objectFit: "none", overflowX: 'auto' }} />
             </div>
         </>
     );
