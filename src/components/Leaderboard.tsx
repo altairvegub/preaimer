09cbf50d68e0fe16a8637f95bb017b0078e8f585
gub <gub@live.ca>
2025-03-04 15:22:59 -0500

    designed landing page and added colour pallet

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,41 +1,43 @@
 function Leaderboard() {
     return (
-        <div className="">
-            <div className="leaderboard">
-                <h2 className="text-white text-2xl tracking-tighter">Leaderboard</h2>
-                <table className="bg-white text-lg">
-                    <thead>
-                        <tr>
-                            <th>Rank</th>
-                            <th>Player</th>
-                            <th>Pixels Off</th>
+        <div className="w-full pb-2 pr-2">
+            <div className="">
+                <h2 className="text-slate-500 text-2xl tracking-tighter text-center">Leaderboard</h2>
+            </div>
+            <div className="rounded-lg overflow-hidden drop-shadow-lg">
+                <table className="text-lg w-full table-fixed border-separate border-spacing-0 rounded-lg">
+                    <thead className="text-left">
+                        <tr className="text-slate-500 bg-secondary">
+                            <th className="w-1/6 text-center rounded-tl-lg">Rank</th>
+                            <th className="w-4/6">Player</th>
+                            <th className="w-1/6">Pixels Off</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Player One</td>
-                            <td>10,500</td>
+                    <tbody className="text-white bg-secondary odd:bg-primary">
+                        <tr className="bg-secondary odd:bg-primary">
+                            <td className="text-center">1</td>
+                            <td>jett</td>
+                            <td className="text-right pr-2">2</td>
                         </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Player Two</td>
-                            <td>9,200</td>
+                        <tr className="bg-secondary odd:bg-primary">
+                            <td className="text-center">2</td>
+                            <td>reyna</td>
+                            <td className="text-right pr-2">10</td>
                         </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Player Three</td>
-                            <td>8,750</td>
+                        <tr className="bg-secondary odd:bg-primary">
+                            <td className="text-center">3</td>
+                            <td>Brim Stone</td>
+                            <td className="text-right pr-2">400</td>
                         </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>Player Four</td>
-                            <td>7,300</td>
+                        <tr className="bg-secondary odd:bg-primary">
+                            <td className="text-center">4</td>
+                            <td>Clove</td>
+                            <td className="text-right pr-2">555</td>
                         </tr>
-                        <tr>
-                            <td>5</td>
-                            <td>Player Five</td>
-                            <td>6,800</td>
+                        <tr className="bg-secondary odd:bg-primary">
+                            <td className="text-center">5</td>
+                            <td>Sova</td>
+                            <td className="text-right pr-2">1099</td>
                         </tr>
                     </tbody>
                 </table>
